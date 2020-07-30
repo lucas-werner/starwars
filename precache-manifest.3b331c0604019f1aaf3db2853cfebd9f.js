719dcfc7afe34b4d3f700f45ebb66fbd8721e3f8
Lucas Werner <lucas.1werner@gmail.com>
2020-07-30 12:48:58 -0300

    Updates

---
+++ precache-manifest.3b331c0604019f1aaf3db2853cfebd9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb18dc4a46edffac8ded21de98bd441",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "401eef78564d37dfd317",
+    "url": "/starwars/static/css/main.81ef3308.chunk.css"
+  },
+  {
+    "revision": "d8a24e34699a90605fb0",
+    "url": "/starwars/static/js/2.1b12210a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwars/static/js/2.1b12210a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "401eef78564d37dfd317",
+    "url": "/starwars/static/js/main.45d7626a.chunk.js"
+  },
+  {
+    "revision": "55549e5ca5646ed78179",
+    "url": "/starwars/static/js/runtime-main.d2cd51f1.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "a4df16589c5640690217f8f95a6b55ef",
+    "url": "/starwars/static/media/space_background2.a4df1658.jpg"
+  },
+  {
+    "revision": "c6f69db5b87bf08d1da243d8f4ef233e",
+    "url": "/starwars/static/media/sw_logo2.c6f69db5.png"
+  }
+]);
